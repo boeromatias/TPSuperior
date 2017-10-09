2ff5bd4a5695253403b22e75b585f5272ccbdde8
boeromatias <boeromatias@gmail.com>
2017-10-09 17:41:11 -0300

    Hyperbola
    Se agrega funcion hyperbola con su grafico.
    En todas las funciones que hice, sean recta, potencial e hyperbola faltan calcular el error que entiendo es lo que se utilizara al final para comparar resultados

---
+++ hiperbola.m
@@ -0,0 +1,39 @@
+%Guardo el archivo en Matriz
+  matriz = load('c:\\temp\\amic\\data.txt');
+%Extraigo las columnas
+  colX=matriz(:,1);
+  colY=matriz(:,2);
+  minX=min(colX);
+  maxX=max(colX);
+%Saco las variables de mis funciones  
+  n=length(colX);
+  sumHipX=sum(1./colX);
+  sumHipY=sum(1./colY);
+  HipX2=(1./colX).^2;
+  sumHipX2=sum(HipX2);
+  HipXY=(1./colX).*(1./colY);
+  sumHipXY=sum(HipXY);
+%Resuelvo el sistema de ecuaciones 
+A = [sumHipX n;sumHipX2 sumHipX];
+B = [sumHipY; sumHipXY];
+X = linsolve(A, B);
+%Cambio de variables
+a=1./X(2);
+b=a.*X(1);
+
+disp(['y =(' num2str(a) ' * x)/(x+' num2str(b) ')']);
+
+%grafico
+  %Genero un vector que todos los valores f(x) de la funcion
+  for i = 1:maxX
+  vecRes(i) = (i*a)/(b+i);
+endfor
+ %Grafico el vector
+  plot(vecRes)
+  
+hold on
+%Grafico la nube de puntos
+for i = 1:n
+  filaI=matriz(i,:);
+  plot(filaI(1),filaI(2));
+  endfor
